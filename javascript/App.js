b3a807b1bd07ba820efb66b88071c558170b62c3
unknown <yeonju.kim2@kt.com>
2023-02-21 18:34:01 +0900

    first commit

---
+++ javascript/App.js
@@ -0,0 +1,23 @@
+import SearchInput from "./components/SearchInput.js"
+import {fetchLanguages } from "./api/api.js"
+
+export default function App({ $target }) {
+  this.state = {
+    fetchedLanguages: [],
+    selectedLanguages: []  
+  }
+
+  this.setState = (nextState) => {
+    // TODO: 구현해야함
+  }
+
+
+// ----초기값과 div 인스턴스 생성----
+  new SearchInput({
+    $target,
+    initialState:""
+  })
+  console.log(fetchLanguages ,"<apiLanguages")
+}
+
+
---
+++ javascript/api/api.js
@@ -0,0 +1,17 @@
+export const API_END_POINT = "API END POINT"
+
+// 요청 api이 언제 요청 되는것인지?
+// 재사용성을 고려해야함
+//fetch하는 request함수와 언어목록을 조회하는 함수를 분리함!\
+
+const request = async(url) =>{
+  const res = await fetch(url)
+  console.log(res,"<res")
+  if(res.ok) {
+    const json = await res.json()
+    return json
+  }
+  throw new Error("요청 실패")
+
+} 
+export const fetchLanguages = async (keyword) => request(`${API_END_POINT}/languages?keyword=${keyword}`)
