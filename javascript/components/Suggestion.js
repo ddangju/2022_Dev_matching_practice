4f655be871e429564478eb9d28334911412a4d03
yeonju <duswn4338@gmail.com>
2023-02-22 23:51:56 +0900

    add: suggestion.js 추가

--- javascript/components/Suggestion.js
+++ javascript/components/Suggestion.js
@@ -2,6 +2,41 @@ export default function Suggestion({ target, initialState }) {
   this.state = initialState;
   this.setState = (nextState) => {
     this.state = nextState;
-    console.log(this.state);
+    this.render();
+  };
+  this.div = document.createElement("div");
+  this.div.className = "Suggestion";
+  target.appendChild(this.div);
+
+  this.render = () => {
+    const { items = [] } = this.state;
+    if (items.length > 0) {
+      this.div.style.display = "block";
+      this.div.innerHTML = `
+      <ul>
+        ${items
+          .map(
+            (item, index) => `
+          <li data-index="${index}">${item}</li>
+        `
+          )
+          .join("")}
+      </ul>
+    `;
+      // console.log(
+      //   items
+      //     .map(
+      //       (item, index) => `
+      //   <li data-index="${index}">${item}</li>
+      // `
+      //     )
+      //     .join("")
+      // );
+
+      //
+    } else {
+      this.div.style.display = "none";
+      this.div.innerHTML = "";
+    }
   };
 }
